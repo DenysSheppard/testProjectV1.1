32db123fc6dedac2a0564a3a1bdcbd183cb7275a
Dennys Sheppard <genus717@gmail.com>
2024-04-18 21:21:27 +0300

    added product function

--- testProjectV1.1/testProjectV1.1.cpp
+++ testProjectV1.1/testProjectV1.1.cpp
@@ -3,9 +3,13 @@
 #include <iostream>
 using namespace std;
 #include "one.h"
+#include "prod.h"
+
 
 int main()
 {
     cout << "Hello World!\n";
     cout << "sum =" << sum(10, 20);
+    cout << "prod =" << prod(10, 20);
+
 }
